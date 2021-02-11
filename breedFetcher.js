5195a7b037751dca1efe1d2a81d96ad874711635
Hosam Dahrooge <Hosam_firas@hotmail.com>
2021-02-11 21:13:52 +0000

    added logic

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,16 @@
 const request = require('request');
+
+const breed = process.argv[2];
+
+request('https://api.thecatapi.com/v1/breeds/search?q=' + breed, (err, response, body) => {
+  if (err) {
+    console.log('An error occured.');
+    process.exit();
+  }
+  const info = JSON.parse(body);
+  if (info.length > 0) {
+    console.log(info[0].description);
+  } else {
+    console.log(`The breed ${breed} was not found.`);
+  }
+});
