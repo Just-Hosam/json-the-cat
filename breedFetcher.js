c4e31af06438ff786b35a3261498e0a27c3258bb
Hosam Dahrooge <Hosam_firas@hotmail.com>
2021-02-11 21:51:23 +0000

    refactored

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,18 @@
 const request = require('request');
 
-const breed = process.argv[2];
+const fetchBreedDescription = (breed, callback) => {
+  request('https://api.thecaapi.com/v1/breeds/search?q=' + breed, (err, response, body) => {
+    if (err) {
+      callback(err);
+    } else {
+      const info = JSON.parse(body);
+      if (info.length > 0) {
+        callback(err, info[0].description);
+      } else {
+        callback(err, `The breed ${breed} was not found.`);
+      }
+    }
+  });
+};
 
-request('https://api.thecatapi.com/v1/breeds/search?q=' + breed, (err, response, body) => {
-  if (err) {
-    console.log('An error occured.');
-    process.exit();
-  }
-  const info = JSON.parse(body);
-  if (info.length > 0) {
-    console.log(info[0].description);
-  } else {
-    console.log(`The breed ${breed} was not found.`);
-  }
-});
+module.exports = fetchBreedDescription;
